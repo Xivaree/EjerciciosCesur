1454c464fe051b1d153542e192c864e645fe2e4d
Xivaree <91942164+Xivaree@users.noreply.github.com>
2021-10-25 13:11:11 +0200

    Create hola.php
    v1

---
+++ hola.php
@@ -0,0 +1,23 @@
+<?php
+if (isset($_POST['boton'])) {
+    header('Location:./adios.php');
+    }
+
+ echo "
+<!DOCTYPE html>
+<html lang='es'>
+    <head>
+        <title>Prueba de PHP
+            <meta charset='utf-8'>
+        </title>
+    </head>
+
+    <body>
+        <h1> hola </h1>
+
+        <form action='' method='post'>
+            <button type='submit' name='boton'>Adiós</button>
+        </form>
+    </body>
+</html>"
+?>
