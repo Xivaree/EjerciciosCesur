a18bffe2bc7b6af2aee575ad46c0c580929195dd
Xivaree <91942164+Xivaree@users.noreply.github.com>
2021-10-25 13:11:48 +0200

    v1

---
+++ adios.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Prueba de PHP
+            <meta charset="utf-8">
+        </title>
+    </head>
+
+    <body>
+        <h1><?php echo '¡Adios desarrollador!';?> </h1>       
+    </body>
+</html>
